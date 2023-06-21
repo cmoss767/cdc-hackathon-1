5e4255fc771ed32f593f8dc22b2127ef6fb85237
austingw <senortinman@gmail.com>
2023-06-21 19:33:34 -0400

    Add some needed packages

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -11,7 +11,7 @@ export default function Native() {
           console.log("Pressed!");
           alert("Pressed!");
         }}
-        text="Boop"
+        text="Press me!"
       />
       <StatusBar style="auto" />
     </View>
