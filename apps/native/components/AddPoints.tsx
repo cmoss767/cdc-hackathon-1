0afcfa0fd03a7800d326a44ed653b2b7e20bcb12
austingw <senortinman@gmail.com>
2023-06-24 14:16:21 -0400

    Placeholder components

---
+++ apps/native/components/AddPoints.tsx
@@ -0,0 +1,17 @@
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  ScrollView,
+  Stack,
+  Text,
+  View,
+} from "native-base";
+import React from "react";
+
+const AddPoints = () => {
+  return <Text>Add Points</Text>;
+};
+
+export default AddPoints;
---
+++ apps/native/components/Leaderboard.tsx
@@ -0,0 +1,17 @@
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  ScrollView,
+  Stack,
+  Text,
+  View,
+} from "native-base";
+import React from "react";
+
+const Leaderboard = () => {
+  return <Text>Leaderboard</Text>;
+};
+
+export default Leaderboard;
