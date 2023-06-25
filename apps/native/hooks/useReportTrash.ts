0fee0ca43b1a9763105e1fd2335cfcdd7024937b
austingw <senortinman@gmail.com>
2023-06-25 16:13:21 -0400

    Minor report trash hooks tweaks

--- apps/native/hooks/useReportTrash.ts
+++ apps/native/hooks/useReportTrash.ts
@@ -1,21 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
-import ApiService from "../api/service";
-import axios from "axios";
+import { useMutation } from "@tanstack/react-query"
+import ApiService from "../api/service"
 
 const reportTrash = async (data: any) => {
-  return await ApiService.post("/report", data)
-    .then()
-    .then((res: any) => res?.data?.data);
-};
+  return await ApiService.post("/report", data).then(
+    (res: any) => res?.data?.data
+  )
+}
 
 function useReportTrash() {
   return useMutation(reportTrash, {
     onSuccess: () => {},
     onError: (error, data) => {
-      console.log("error", data);
-      console.log(error);
+      console.log("error", data)
+      console.log(error)
     },
-  });
+  })
 }
 
-export default useReportTrash;
+export default useReportTrash
