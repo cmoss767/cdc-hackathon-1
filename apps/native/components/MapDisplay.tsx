a100054d6d1877ff99dd21e0bf4cf37963d4c946
austingw <senortinman@gmail.com>
2023-06-25 16:12:15 -0400

    Update map and add hook to display user reports

--- apps/native/components/MapDisplay.tsx
+++ apps/native/components/MapDisplay.tsx
@@ -1,9 +1,12 @@
 import React from "react"
 import MapView, { Marker } from "react-native-maps"
 import { StyleSheet } from "react-native"
+import useGetTrashReports from "../hooks/useGetTrashReports"
+import { HStack, Spinner, Text } from "native-base"
 
 interface MapDisplayProps {
   userLocation: any
+  helpLocation: any
 }
 
 const MapDisplay = ({ userLocation }: MapDisplayProps) => {
@@ -14,22 +17,81 @@ const MapDisplay = ({ userLocation }: MapDisplayProps) => {
     },
   })
 
+  const { data, isLoading, isError } = useGetTrashReports()
+
+  const trashReports = data
+    ?.filter((trash: any) => trash.report === false)
+    ?.map((trash: any) => {
+      return trash.location.latitude && trash.location.longitude ? (
+        <Marker
+          key={trash.id}
+          coordinate={{
+            latitude: trash?.location?.latitude,
+            longitude: trash?.location?.longitude,
+          }}
+          title="Trash Reported! 🚯"
+          description={`${trash.name ? trash.name : "A user"} reported ${
+            trash.amount
+          } ${trash.amount > 1 ? "pieces" : "piece"} of trash here! 😱`}
+          pinColor={
+            trash.severity === "LOW"
+              ? "#FFCF00"
+              : trash.severity === "MEDIUM"
+              ? "#EE6123"
+              : "#FA003F"
+          }
+        />
+      ) : null
+    })
+
+  const pickupReports = data
+    ?.filter((trash: any) => trash.report === true)
+    ?.map((trash: any) => {
+      return trash.location.latitude && trash.location.longitude ? (
+        <Marker
+          key={trash.id}
+          coordinate={{
+            latitude: trash?.location?.latitude,
+            longitude: trash?.location?.longitude,
+          }}
+          title="Trash Picked Up! 🙌"
+          description={`${
+            trash.name ? trash.name : "An anonymous user"
+          } picked up ${trash.amount} ${
+            trash.amount > 1 ? "pieces" : "piece"
+          } of trash here! 🥳`}
+          pinColor="#FEEFE5"
+        />
+      ) : null
+    })
+
   return (
-    <MapView
-      style={styles.map}
-      region={{
-        latitude: userLocation?.coords.latitude,
-        longitude: userLocation?.coords.longitude,
-        latitudeDelta: 0.04,
-        longitudeDelta: 0.02,
-      }}
-    >
-      <Marker
-        coordinate={userLocation?.coords}
-        title={"Your Location"}
-        pinColor="#000000"
-      />
-    </MapView>
+    <>
+      {userLocation && !isLoading ? (
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: userLocation?.coords.latitude,
+            longitude: userLocation?.coords.longitude,
+            latitudeDelta: 0.04,
+            longitudeDelta: 0.02,
+          }}
+        >
+          <Marker
+            coordinate={userLocation?.coords}
+            title={"Your Location"}
+            pinColor="#00916E"
+          />
+          {trashReports}
+          {pickupReports}
+        </MapView>
+      ) : (
+        <HStack alignItems={"center"} justifyContent={"center"}>
+          <Spinner />
+          <Text>Loading location... 🌎</Text>
+        </HStack>
+      )}
+    </>
   )
 }
 
