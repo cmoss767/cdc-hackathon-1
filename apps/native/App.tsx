0e259d530039ad9813dad8a7f650006c138c0449
austingw <senortinman@gmail.com>
2023-06-25 19:18:41 -0400

    Add theme to app

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,16 +1,35 @@
-import { NativeBaseProvider } from "native-base";
-import Home from "./screens/Home";
-import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NativeBaseProvider, extendTheme } from "native-base"
+import Home from "./screens/Home"
+import React from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export default function Native() {
+  const theme = extendTheme({
+    colors: {
+      green: "#BFCC94",
+      white: "#F0F4EF",
+      pink: "#E6AACE",
+      blue: "#344966",
+      black: "#0D1821",
+      primary: { 600: "#344966", 700: "#E6AACE", 800: "#0D1821" },
+      singletons: {
+        darkText: "#F0F4EF",
+        lightText: "#0D1821",
+      },
+      config: {
+        // Changing initialColorMode to 'dark'
+        initialColorMode: "dark",
+      },
+    },
+  })
+
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <Home />
       </QueryClientProvider>
     </NativeBaseProvider>
-  );
+  )
 }
