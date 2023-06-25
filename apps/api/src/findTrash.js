89e3b2da00b0791b0dbcbfc9b96cf738ad9101b1
Chris Moss <hcmoss70@gmail.com>
2023-06-25 15:41:33 -0400

    updated endpoint

--- apps/api/src/findTrash.js
+++ apps/api/src/findTrash.js
@@ -1,14 +1,46 @@
 import { kmeans } from "ml-kmeans"
-import { coordinates } from "./dummyData"
+import { PrismaClient } from "@prisma/client"
 
 const findTrash = async (req, res) => {
   try {
-    const numClusters = 5 // Adjust the number of clusters as needed
+    const prisma = new PrismaClient()
+    const trash = await prisma.trash.findMany()
 
-    const { centroids, clusters } = kmeans(coordinates, numClusters, {
+    const coordinates = []
+
+    //reshaping coords [[lat, long]]
+    trash.forEach((trashItem) => {
+      coordinates.push([
+        trashItem.location.latitude,
+        trashItem.location.longitude,
+      ])
+    })
+
+    const numClusters = 5
+
+    //getting 5 centers of the clusters
+    const { centroids } = kmeans(coordinates, numClusters, {
       maxIterations: 100,
       tolerance: 1e-6,
     })
+
+    const calculateDistance = (lat1, lon1, lat2, lon2) => {
+      const R = 3958.8 // Radius of the Earth in miles
+
+      const latDiff = (lat2 - lat1) * (Math.PI / 180)
+      const lonDiff = (lon2 - lon1) * (Math.PI / 180)
+
+      const a =
+        Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
+        Math.cos(lat1 * (Math.PI / 180)) *
+          Math.cos(lat2 * (Math.PI / 180)) *
+          Math.sin(lonDiff / 2) *
+          Math.sin(lonDiff / 2)
+
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+      return R * c
+    }
 
     // Calculate the distances and find the centroid with the highest concentration
     let highestConcentrationClusterSize = 0
@@ -26,42 +58,29 @@ const findTrash = async (req, res) => {
           centroid[1]
         )
 
-        if (distanceInMiles <= 50) {
+        if (distanceInMiles <= 5) {
           clusterSize++
         }
       }
+      console.log(clusterSize, "clusterSize")
 
       if (clusterSize > highestConcentrationClusterSize) {
         highestConcentrationClusterSize = clusterSize
         highestConcentrationClusterIndex = i
       }
+      console.log(
+        highestConcentrationClusterSize,
+        "highestConcentrationClusterSize"
+      )
     }
     const highestConcentrationCentroid =
       centroids[highestConcentrationClusterIndex]
-    let result = 0
 
     res.json(highestConcentrationCentroid)
   } catch (error) {
     console.log(error)
     res.status(500).json({ error: "An error occurred" })
   }
-}
-const calculateDistance = (lat1, lon1, lat2, lon2) => {
-  const R = 3958.8 // Radius of the Earth in miles
-
-  const latDiff = (lat2 - lat1) * (Math.PI / 180)
-  const lonDiff = (lon2 - lon1) * (Math.PI / 180)
-
-  const a =
-    Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
-    Math.cos(lat1 * (Math.PI / 180)) *
-      Math.cos(lat2 * (Math.PI / 180)) *
-      Math.sin(lonDiff / 2) *
-      Math.sin(lonDiff / 2)
-
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-
-  return R * c
 }
 
 export default findTrash
